ff51a640ca939e71119eda1dc9e92a4d58468007
unknown <godhanidivyesh44@gmail.com>
2023-08-24 21:01:16 +0530

    05_tailwindprops react app add

---
+++ 05_taillwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import { CardOne } from "./components/Team"
+
+function App() {
+
+  return (
+    <>
+    <h1 className="bg-red-400 rounded-md">Tailwind</h1>
+    <CardOne username = "dgpatel" btntext = "Click Now" />
+    <CardOne username = "agpatel" btntext = "Click Now" />
+    </>
+  )
+}
+
+export default App
