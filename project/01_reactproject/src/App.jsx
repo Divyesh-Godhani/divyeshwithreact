569794d011a0a025f0024f7dda7c07376d04779e
unknown <godhanidivyesh44@gmail.com>
2023-08-25 11:41:19 +0530

    add project

---
+++ project/01_reactproject/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button"
+
+
+function App() {
+
+  return (
+    <div style={{display:"flex"}}>
+      <Button title="App Store"/>
+      <Button title="Play Store"/>
+    </div>
+  )
+}
+
+export default App
